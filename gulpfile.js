bee0b8c4aa8de008534482c32b6b304f857b25f6
Gabriella Navarro <navarro.gabriella@gmail.com>
2018-07-20 15:27:05 -0300

    Insere bônus accordion

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 gulp.task('sass', () => {
   gulp.src('./scss/**/*.scss')
@@ -19,12 +21,21 @@ gulp.task('sass:watch', () => {
   gulp.watch('./scss/**/*.scss', ['sass']);
 });
 
-gulp.task('watch', () => {
-  gulp.run('sass:watch');
+gulp.task('js', () => {
+  let components = [
+  ];
+  let jsDest = './assets/js/';
+
+  gulp.src(components)
+    .pipe(concat('components.js'))
+    .pipe(gulp.dest(jsDest))
+    .pipe(rename('components-min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest));
 });
 
-gulp.task('build', () => {
-  gulp.run('sass');
-});
+gulp.task('watch', ['sass:watch']);
+
+gulp.task('build', ['sass', 'js']);
 
 gulp.task('default', ['build', 'watch']);
